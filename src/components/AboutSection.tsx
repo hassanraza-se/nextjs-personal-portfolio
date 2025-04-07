43a564bcb91ad9d3ed18251e29f481eb010aaa55
hassanrazadev <hr7886@gmail.com>
2025-04-07 15:00:26 +0500

    insert import for JSX

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {JSX} from 'react';
 import Button from "@/components/Button";
 import Image from "next/image";
 import SocialLinks from "@/components/SocialLinks";
